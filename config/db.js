8ea53196c7b8a69fa32951bb46ae522049317d24
RAJDEEKSHITH <forworkpurpose2912@gmail.com>
2026-09-16 18:15:18 +0530

    fix dns lookup issue

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
+import dns from "node:dns";
 import mongoose from "mongoose";
-
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,8 @@
 import { createClient } from "redis";
 const redisUrl = process.env.REDIS_URL
+
 const redisClient = createClient({
-  url : redisUrl
+  url : redisUrl,
 });
 
 redisClient.on("error", (err) => {
